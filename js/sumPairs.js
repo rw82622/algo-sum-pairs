a14758740e753580606bbb19bd692d7264e8096b
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-26 14:38:29 -0500

    js solution for sum_pairs

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,9 @@
-exports.sumPairs = function() {
-
+exports.sumPairs = function (arr, target) {
+  let res = [];
+  for (let currentIndex = 0; currentIndex < arr.length; currentIndex++) {
+    if (arr.indexOf(target - arr[currentIndex], currentIndex + 1) > 0) {
+      res.push([arr[currentIndex], target - arr[currentIndex]]);
+    }
+  }
+  return res;
 };
--- js/sumPairsSpec.js
+++ js/sumPairsSpec.js
@@ -1,3 +1,5 @@
 var sp = require("./sumPairs");
+var shallowEqualArrays = require("shallow-equal/arrays");
 
 // Don't forget to add your tests :)
+console.log(shallowEqualArrays(sp.sumPairs([1, 2, 3, 4, 5], 9), [[4, 5]]));
