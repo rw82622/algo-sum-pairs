cc6542a660d1c3277c1d77798081525595995563
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-26 14:52:09 -0500

    fixed Spec file

--- js/sumPairsSpec.js
+++ js/sumPairsSpec.js
@@ -2,4 +2,9 @@ var sp = require("./sumPairs");
 var shallowEqualArrays = require("shallow-equal/arrays");
 
 // Don't forget to add your tests :)
-console.log(shallowEqualArrays(sp.sumPairs([1, 2, 3, 4, 5], 9), [[4, 5]]));
+console.log(
+  shallowEqualArrays(
+    JSON.stringify(sp.sumPairs([1, 2, 3, 4, 5], 9)),
+    JSON.stringify([[4, 5]])
+  )
+);
