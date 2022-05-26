448a18c283097f15c00e45b42861433886ab9aea
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-26 15:05:56 -0500

    python solution for sum_pairs

--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,7 @@
-def sum_pairs():
+def sum_pairs(arr, target):
+    finalArray = []
+    for currentIndex in range(len(arr)):
+        if target - arr[currentIndex] in arr[currentIndex+1:]:
+            finalArray.append([arr[currentIndex], target - arr[currentIndex]])
+    return finalArray
+
--- python/sum_pairs_spec.py
+++ python/sum_pairs_spec.py
@@ -1,3 +1,4 @@
 from sum_pairs import sum_pairs
 
 # write your specs here!
+print(sum_pairs([1,2,3,4,5], 9) == [[4,5]])
